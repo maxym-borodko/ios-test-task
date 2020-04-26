089ef28a4aa0aa2fe13485e4e14492f68db96247
Maxym Borodko <max.borodko@gmail.com>
2020-04-26 19:21:25 +0300

    added base UI; base project structure created

---
+++ test-ios-app/views/AlarmTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlarmTableViewCell.swift
+//  test-ios-app
+//
+//  Created by Maxym Borodko on 26.04.2020.
+//  Copyright © 2020 Maxym Borodko. All rights reserved.
+//
+
+import UIKit
+
+class AlarmTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ test-ios-app/views/AlarmViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  test-ios-app
+//
+//  Created by Maxym Borodko on 26.04.2020.
+//  Copyright © 2020 Maxym Borodko. All rights reserved.
+//
+
+import UIKit
+
+class AlarmViewController: UIViewController {
+    
+    @IBOutlet var stateLabel: UILabel!
+    @IBOutlet var sleepTimeButton: UIButton!
+    @IBOutlet var alarmButton: UIButton!
+    @IBOutlet var changeStateButton: UIButton!
+    
+    // MARK: - UIViewController lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Actions
+    @IBAction func onChangeSleepTime() {
+        
+    }
+    
+    @IBAction func onChangeAlarmTime() {
+        
+    }
+    
+    @IBAction func onChangeState() {
+        
+    }
+}
+
